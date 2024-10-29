43780cd3e9ee0e1c4e44c11959f46e6b0dc84da2
0nyinye <onyinyeekezi2017@gmail.com>
2024-10-29 11:54:04 +0100

    completed assignment

--- Pattern.java
+++ Pattern.java
@@ -4,12 +4,18 @@ public static void main(String[] args)  {
 
 
 for(int index=1; index<=6; index++)  {
-  for(int count=1; count<=index; count++);
+  System.out.println();
+	for(int count=1; count<=index; count++)
 
 System.out.print("*");
-System.out.println();
 
+for(int index=6; index<=1; index++)  {
+  System.out.println();
+	for(int count=1; count<=index; count++)
 
+System.out.print("*");
+
+}
 }
 }
 }
--- VowelOrConsonant.java
+++ VowelOrConsonant.java
@@ -6,18 +6,18 @@ public class VowelOrConsonant  {
     Scanner input = new Scanner(System.in);
     while(true)  {
     System.out.print("Enter an alphabet: ");
-    String input = input.charAt();
+    String alphabet = input.nextLine();
     
-    if(input.length() !=1)  {
+    if(alphabet.length() !=1)  {
       System.out.println("Error. input should be a single alphabet" );
-    }else if(input != ("[a-zA-Z]")) {
+    }else if(!alphabet.equals("a") && !alphabet.equals("b") && !alphabet.equals("c") && !alphabet.equals("d") && !alphabet.equals("e") && !alphabet.equals("f") && !alphabet.equals("g") && !alphabet.equals("h") && !alphabet.equals("i") && !alphabet.equals("j") && !alphabet.equals("k") && !alphabet.equals("l") && !alphabet.equals("m") && !alphabet.equals("n") && !alphabet.equals("o") && !alphabet.equals("p") && !alphabet.equals("q") && !alphabet.equals("r") && !alphabet.equals("s") && !alphabet.equals("t") && !alphabet.equals("u") && !alphabet.equals("v") && !alphabet.equals("w") && !alphabet.equals("x") && !alphabet.equals("y") && !alphabet.equals("z")) {
       System.out.println("Error. input should be a letter" );
     }else {
-     char letter = input.charAt(); 
+     char letter = alphabet.charAt(0); 
     if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
-      System.out.println(input + "is a vowel");
+      System.out.println(alphabet + " is a vowel");
     }else{
-      System.out.println(input + "is a consonant");
+      System.out.println(alphabet + " is a consonant");
     }
 
 
