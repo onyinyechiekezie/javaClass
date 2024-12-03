57d6e1158df01216352ec0b522240bb4e40ac33c
0nyinye <onyinyeekezi2017@gmail.com>
2024-12-03 14:55:54 +0100

    Update on Student Grade

--- PhaseGate(java)/StudentGrades.java
+++ PhaseGate(java)/StudentGrades.java
@@ -5,12 +5,17 @@ public class StudentGrades  {
 
 		Scanner input = new Scanner(System.in);
 		int[] scores;
+		int[]total;
+		int totals = 0;
 		int[] numberOfStudent;
+		//int[] total = new int[numberOfStudents];
 		System.out.print("How many students do you have?\n");
 		int numberOfStudents = input.nextInt();
 		System.out.print("How many subjects do they offer?\n");
 		int numberOfSubjects = input.nextInt();
 		numberOfStudent = new int[numberOfStudents];
+		
+	
 		printSave();
 	 	
 
@@ -19,25 +24,41 @@ public class StudentGrades  {
 				System.out.print("Entering Score For Student"+index+"\nEnter score for subject"+count+"\n");
 				int score = input.nextInt();
 				printSave();
-				score = score + index;
+				System.out.println();
+				totals = totals + score;
+				System.out.print(totals);
 				scores = new int[score];
 			
-			}
-		} 
-		for(int index = 1; index <=numberOfStudents.length; index++) {
-			numberOfStudent[index] = numberOfStudents[index];
-		}
+			//}
+		//} 
+		for(int i = 0; i < numberOfStudents - 1; i++) {
+			numberOfStudent[i] = numberOfStudent[numberOfStudents -1];
+			numberOfStudent[numberOfStudents -1] = numberOfStudent[numberOfStudents - 1];
+			
+		
 		printLine();	
 		System.out.printf("%s\t\t%s\t%s\t%s\t%s\t%s\t\t%s%n","STUDENT","SUB1","SUB2","SUB3","TOT","AVERAGE","POS");
 		printLine();
-		System.out.printf("%s","Student",numberOfStudent[0] );
+		//for(int j =0; j<=scores.length;j++)
+		System.out.printf("%s\t\t%s%n","Student"+index+"" );
 			
+		}
+		}
+		}	
 	}	
 	public static void printSave(){
 		System.out.print("Saving >>>>>>>>>>>>>>>>>>>>>\nSaved successfully\n");
 	}
 	public static void printLine(){
-		System.out.print("========================================================\n");
+		System.out.print("==============================================================================\n");
 	}
 
-}
+}//student number[index] = input.next
+//newi=studentnumber[index]
+
+//int[][]=new int[numberOfstudents][subject]
+//int[]total= new int[studentnumber]
+//int[] average= new int[numberofstudent]
+//for(int count = 1; count <+numbers of student.length; count++)  {
+	//System.out.print
+
