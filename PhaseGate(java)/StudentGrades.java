d0a4a74bb02fe167781856e1330e062fdb4de7ab
0nyinye <onyinyeekezi2017@gmail.com>
2024-12-03 12:30:49 +0100

    Update on Student Grade

--- PhaseGate(java)/StudentGrades.java
+++ PhaseGate(java)/StudentGrades.java
@@ -4,22 +4,38 @@ public class StudentGrades  {
 	public static void main(String[] args)  {
 
 		Scanner input = new Scanner(System.in);
-
-		System.out.print("How many students do you have?:");
-		int[] numberOfStudents = input.nextInt();
-		System.out.print("How many subjects do they offer?:");
-		int[] numberOfSubjects = input.nextInt();
-		int[] numberOfStudents = new int[numberOfStudents];
-		int[] numberOfSubjects = new int[numberOfSubjects];
-	 
+		int[] scores;
+		int[] numberOfStudent;
+		System.out.print("How many students do you have?\n");
+		int numberOfStudents = input.nextInt();
+		System.out.print("How many subjects do they offer?\n");
+		int numberOfSubjects = input.nextInt();
+		numberOfStudent = new int[numberOfStudents];
+		printSave();
+	 	
 
 		for(int index = 1; index<=numberOfStudents; index++){
 			for(int count = 1; count <=numberOfSubjects; count++){
-				System.out.print("Entering Score For Student"+index+"\nEnter score for subject"+count+" :");
+				System.out.print("Entering Score For Student"+index+"\nEnter score for subject"+count+"\n");
 				int score = input.nextInt();
+				printSave();
+				score = score + index;
+				scores = new int[score];
+			
 			}
-		}
-
+		
+		input.nextLine();
+		printLine();	
+		System.out.printf("%s\t\t%s\t%s\t%s\t%s\t%s\t\t%s%n","STUDENT","SUB1","SUB2","SUB3","TOT","AVERAGE","POS");
+		printLine();
+		System.out.printf("%s","Student",numberOfStudents[0] );
+		}	
+	}	
+	public static void printSave(){
+		System.out.print("Saving >>>>>>>>>>>>>>>>>>>>>\nSaved successfully\n");
+	}
+	public static void printLine(){
+		System.out.print("========================================================\n");
 	}
 
 }
