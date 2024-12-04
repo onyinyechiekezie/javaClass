5be61f20a00feff0bd14ed7da769dd474c2e0566
0nyinye <onyinyeekezi2017@gmail.com>
2024-12-04 08:37:01 +0100

    update on student grades

--- PhaseGate(java)/StudentGrades.java
+++ PhaseGate(java)/StudentGrades.java
@@ -1,64 +1,67 @@
 import java.util.Arrays;
 import java.util.Scanner;
-public class StudentGrades  {
-	public static void main(String[] args)  {
 
-		Scanner input = new Scanner(System.in);
-		int[] scores;
-		int[]total;
-		int totals = 0;
-		int[] numberOfStudent;
-		//int[] total = new int[numberOfStudents];
-		System.out.print("How many students do you have?\n");
-		int numberOfStudents = input.nextInt();
-		System.out.print("How many subjects do they offer?\n");
-		int numberOfSubjects = input.nextInt();
-		numberOfStudent = new int[numberOfStudents];
-		
-	
-		printSave();
-	 	
+public class StudentGrades {
+    public static void main(String[] args) {
 
-		for(int index = 1; index<=numberOfStudents; index++){
-			for(int count = 1; count <=numberOfSubjects; count++){
-				System.out.print("Entering Score For Student"+index+"\nEnter score for subject"+count+"\n");
-				int score = input.nextInt();
-				printSave();
-				System.out.println();
-				totals = totals + score;
-				System.out.print(totals);
-				scores = new int[score];
-			
-			//}
-		//} 
-		for(int i = 0; i < numberOfStudents - 1; i++) {
-			numberOfStudent[i] = numberOfStudent[numberOfStudents -1];
-			numberOfStudent[numberOfStudents -1] = numberOfStudent[numberOfStudents - 1];
-			
-		
-		printLine();	
-		System.out.printf("%s\t\t%s\t%s\t%s\t%s\t%s\t\t%s%n","STUDENT","SUB1","SUB2","SUB3","TOT","AVERAGE","POS");
-		printLine();
-		//for(int j =0; j<=scores.length;j++)
-		System.out.printf("%s\t\t%s%n","Student"+index+"" );
-			
-		}
-		}
-		}	
-	}	
-	public static void printSave(){
-		System.out.print("Saving >>>>>>>>>>>>>>>>>>>>>\nSaved successfully\n");
-	}
-	public static void printLine(){
-		System.out.print("==============================================================================\n");
-	}
+        Scanner input = new Scanner(System.in);
 
-}//student number[index] = input.next
-//newi=studentnumber[index]
+        System.out.print("How many students do you have?\n");
+        int numberOfStudents = input.nextInt();
+        System.out.print("How many subjects do they offer?\n");
+        int numberOfSubjects = input.nextInt();
+        printSave();
 
-//int[][]=new int[numberOfstudents][subject]
-//int[]total= new int[studentnumber]
-//int[] average= new int[numberofstudent]
-//for(int count = 1; count <+numbers of student.length; count++)  {
-	//System.out.print
+        int[][] scores = new int[numberOfStudents][numberOfSubjects];
+        int[] total = new int[numberOfStudents];
+        double[] average = new double[numberOfStudents];
+	int [] sortedArray = new int[numberOfStudents]; 
 
+        for (int index = 0; index < scores.length; index++) {
+            System.out.println("Entering scores for student " + (index + 1));
+            for (int counter = 0; counter < scores[index].length; counter++) {
+                System.out.println("Enter score for subject " + (counter + 1) + ":");
+                scores[index][counter] = input.nextInt();
+                printSave();
+            }
+        }
+
+        for (int index = 0; index < scores.length; index++) {
+            int sum = 0;
+            for (int counter = 0; counter < scores[index].length; counter++) {
+                sum += scores[index][counter];
+            }
+            total[index] = sum;
+	    sortedArray[index] = sum;
+            average[index] = (double) sum / numberOfSubjects;
+        }
+
+     
+
+        printLine();
+        System.out.printf("%9s", "STUDENT");
+        for (int index = 1; index <= numberOfSubjects; index++) {
+            System.out.printf("\t%s", "SUB" + index);
+        }
+        System.out.printf("\t%s\t%s\t%s%n", "TOTAL", "AVERAGE", "POSITION");
+        printLine();
+
+        for (int index = 0; index < scores.length; index++) {
+            System.out.printf("%-10s", "Student " + (index + 1));
+            for (int counter = 0; counter < scores[index].length; counter++) {
+                System.out.printf("\t%d", scores[index][counter]);
+            }
+            System.out.printf("\t%d\t%.2f%n", total[index], average[index]);
+        }
+
+        printLine();
+    }
+
+    public static void printLine() {
+        System.out.print("==========================================================================\n");
+    }
+
+    public static void printSave() {
+        System.out.print("Saving >>>>>>>>>>>>>>>>>>>\nSaved successfully\n");
+    }
+}
